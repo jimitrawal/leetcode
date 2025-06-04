b5b0a5c3109558ab56e1ebbd302969207af69f52
Jimit Rawal <144960502+jimitrawal@users.noreply.github.com>
2025-06-04 10:44:54 -0400

    Update solution for two-sum

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -1,6 +1,9 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
-    }
-};
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mt=[]
+        for x in range(len(nums)):
+            for y in range(x+1,len(nums)):
+                if nums[x]+nums[y]==target:
+                    mt.append(x)
+                    mt.append(y)
+        return mt
